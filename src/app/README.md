3eb70e39afbddda7f5566dcb57733ca3b92bbaaf
coltonsteeve <coltonsteeve@gmail.com>
2019-03-21 23:04:27 -0500

    Chat app converted to Angular

---
+++ src/app/README.md
@@ -0,0 +1,74 @@
+# node-express-chat
+
+> Node.js and Socket.io make full-duplex communication easy
+> (full 2-way, like a cell phone conversation)
+
+## Links
+
+- [Demo](https://denisecase.github.io/node-express-chat/)
+- [Source](https://github.com/denisecase/node-express-chat)
+
+## Requirements
+
+- A browser (e.g., Chrome)
+- A text editor (e.g., VS Code, or Notepad++, or Chrome)
+
+## Benefits
+
+- Folder-based
+- Easy to write
+- Node.js non-blocking event loop supports many concurrent requests
+- Socket.io makes full duplex communication easy
+
+## Prerequisites
+
+- [node-server](https://github.com/denisecase/node-server)
+- [node-express-app](https://github.com/denisecase/node-express-app)
+
+## Install Dependencies Listed in package.json
+
+Open PowerShell here as admin and run: `npm install`. Verify node_modules was created. Review the contents.
+
+## Start your Server
+
+Open PowerShell here as admin and start your app with nodemon: `nodemon app.js`
+
+## Open a Browser Client
+
+1. Open a web browser.
+2. Go to the URL: <http://127.0.0.1:3003/> or <http://localhost:3003/>.
+
+## Use PowerShell and ipconfig to get your IP address
+
+1. Open PowerShell as Admin, run ipconfig.
+1. Find your IPv4 address.
+1. Invite others to interact with your server-side app.
+
+## Terms
+
+- full duplex
+- Socket.io
+- npm install
+- node_modules
+- package-lock.json
+
+## Reference
+
+- [Create Simple Chat App Using Node.js, Express, and Socket.io](http://javabeginnerstutorial.com/javascript-2/create-simple-chat-application-using-node-js-express-js-socket-io/)
+
+## Resources
+
+- [Bootstrap Material Design CDN](https://mdbootstrap.com/md-bootstrap-cdn/)
+- [HTML Validator](https://validator.w3.org/)
+- [JavaScript Standard Style Validator](https://standardjs.com/demo.html)
+
+## See Also
+
+- [js-console](https://github.com/profcase/js-console)
+- [js-gui](https://github.com/profcase/js-gui)
+- [js-gui-vue](https://github.com/denisecase/js-gui-vue)
+- [js-gui-storage](https://github.com/profcase/js-gui-storage)
+- [node-server](https://github.com/denisecase/node-server)
+- [node-express-app](https://github.com/denisecase/node-express-app)
+- [node-express-chat](https://github.com/denisecase/node-express-chat)
+- [node-express-mvc](https://github.com/denisecase/node-express-mvc)
---
+++ src/app/public/chat.js
@@ -0,0 +1,106 @@
+const socket = io()
+
+// initialize:
+// Code in document ready or DOMContentLoaded will only run once (DOM) is ready for JavaScript code to execute. 
+// Code in window load will run once the entire page (images or iframes), not just the DOM, is ready.
+
+window.addEventListener('load', () => {
+  console.log('Entering window.load')
+
+  // access important elements in code............
+  const formElement = $("#chatform")[0];  // jQuery CSS selectors return an array - use the first
+  const messageElement = $("#m")[0];      // jQuery CSS selectors return an array - use the first
+
+  // configure event listeners (use action - NOT ON action)......
+  formElement.addEventListener('submit', submitfunction);   // onsubmit = "return submitfunction();"
+  messageElement.addEventListener('keyup', notifyTyping);   // onkeyup = "notifyTyping();"
+  /*formElement.addEventListener('submit', function(){
+    insertImage();
+  });*/
+
+  // additional initialization.........
+  const name = makeid();
+  $('#user').val(name);
+  // emit a chatMessage event from the System along with a message
+  socket.emit('chatMessage', 'System', '<b>' + name + '</b> has joined the discussion');
+
+  console.log('Exiting window.load')
+})
+
+// utility function to create a new random user name....
+function makeid() {
+  let text = ''
+  text = prompt("What is your name?");
+  /*const possible = 'abcdeghijklmnoprstuwxy'
+  for (let i = 0; i < 5; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length))
+  }*/
+  return text
+}
+
+
+// emit a new chatMessage event from the client......
+function submitfunction() {
+  let from = $('#user').val()
+  let message = $('#m').val()
+  if (message !== '') {
+    if (message.indexOf("/image") == 0) {
+      message = message.substring(6,message.length);
+      message = '<img src="' + message + '" height=50% width=50%>';
+      socket.emit('chatMessage', from, message);
+    }
+    else if (message.indexOf("/link") == 0) {
+      message = message.substring(5,message.length);
+      message = '<a href="' + message + '">' + message + '</a>';
+      socket.emit('chatMessage', from, message); 
+    }
+    else if (message.indexOf("/bold") == 0) {
+      message = message.substring(5,message.length);
+      message = '<b>' + message + '</b>';
+      socket.emit('chatMessage', from, message); 
+    }
+    else {
+      socket.emit('chatMessage', from, message);
+    }
+  }
+    
+  // what language and selector is used below?
+  // set the value to an empty string and
+  // focus on the message box again
+  $('#m').val('').focus()
+  return false; // don't refresh
+}
+
+// emit a new notifyUser event from the client.........
+function notifyTyping() {
+  let user = $('#user').val()
+  socket.emit('notifyUser', user)
+}
+
+//Insert Image
+/*function insertImage() {
+  if ($("input:first").val() === "Hello") {
+    alert("Hello");
+  }
+}*/
+
+// how to react to a chatMessage event.................
+socket.on('chatMessage', function (from, msg) {
+  const me = $('#user').val()
+  const color = (from === me) ? 'green' : '#009afd'
+  from = (from === me) ? 'Me' : from
+  $('#messages').append('<li><b style="color:' + color + '">' + from + '</b>: ' + msg + '</li>')
+})
+
+// how to react to a notifyUser event.................
+socket.on('notifyUser', function (user) {
+  const me = $('#user').val()
+  if (user !== me) {
+    $('#notifyUser').text(user + ' is typing ...')
+  }
+  // 10 seconds after typing stops, set the notify text to an empty string
+  setTimeout(function () { $('#notifyUser').text('') }, 10000)
+})
+
+
+
